4e65c36a13b77dc675254b27686d560f745c4f4d
Eugene Hyrol <jekahy@gmail.com>
2012-03-09 19:25:49 +0100

    Initial Commit

---
+++ organizerdb/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  organizerdb
+//
+//  Created by Жека Г on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
